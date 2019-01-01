8430d4032b84aaecb3187333fc632d2e4df0ea20
Deepthi <deepthi.arunn@gmail.com>
2018-12-31 22:04:13 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ document.addEventListener('click',function(event){
     event = event || window.event;
     if (!event.target) event.target = event.srcElement;
     let clickedElement = event.target;
+    var myElement = document.getElementById("#"+clickedElement.id);
+
+    var topPos = myElement.offsetTop + 0;
+    console.log(clickedElement.id)
+    console.log(topPos)
+    document.getElementById('main-content').scrollTop = topPos;
 
     let navs = document.getElementsByClassName('nav-li');
     for(var i = 0; i < navs.length ; i++)
@@ -16,7 +22,7 @@ document.addEventListener('click',function(event){
 
     if (clickedElement.classList.contains("nav-li")){ 
             clickedElement.classList.toggle("active")
-        window.location=clickedElement.id;
-        return;
+        // window.location=clickedElement.id;
     }
+    return;
 })
